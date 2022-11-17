60cfdb703274350d98f1194292ae399dd6576bfa
tons0trees <forrest@fmcclure.com>
2022-11-17 10:31:10 -0700

    found the bug

--- src/components/ActivityItem.jsx
+++ src/components/ActivityItem.jsx
@@ -1,28 +1,33 @@
 import React from "react";
-import { deleteActivity } from "../api"
-
+import { deleteActivity } from "../api";
 
 const ActivityItem = ({ activity, isOwner, list, setList }) => {
-
-  async function handleDelete(event) {
-    event.preventDefault()
-    const deletedActivity = await deleteActivity({activity})
-    const newList = [...list]
-    const routine = newList.find((elem)=> elem.id === activity.routineId)
-    const newActivityList = routine.activities.filter((elem)=> activity.id != elem.id)
-    routine.activities = newActivityList
-    setList(newList)
-  }
-  return (
-    <div className="activity_item">
-        <b>{activity.name}</b>
-        <p>{activity.description}</p>
-        <span> duration: {activity.duration},  count: {activity.count}</span>
-        <div>
-          {isOwner ? <button onClick={handleDelete}>DELETE</button> : null}
+    async function handleDelete(event) {
+        event.preventDefault();
+        await deleteActivity(activity);
+        const newList = [...list];
+        const routine = newList.find((elem) => elem.id === activity.routineId);
+        const newActivityList = routine.activities.filter(
+            (elem) => activity.id != elem.id
+        );
+        routine.activities = newActivityList;
+        setList(newList);
+    }
+    return (
+        <div className="activity_item">
+            <b>{activity.name}</b>
+            <p>{activity.description}</p>
+            <span>
+                {" "}
+                duration: {activity.duration}, count: {activity.count}
+            </span>
+            <div>
+                {isOwner ? (
+                    <button onClick={handleDelete}>DELETE</button>
+                ) : null}
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default ActivityItem;
